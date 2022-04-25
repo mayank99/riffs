764327b2f26db56a53f9814b400cce187c2bc9f1
Mayank <mayank99@users.noreply.github.com>
2022-04-24 23:22:13 -0400

    convert to mp3 first

--- app/routes/resource/audiowaveform.ts
+++ app/routes/resource/audiowaveform.ts
@@ -2,22 +2,29 @@ import type { LoaderFunction } from 'remix';
 import os from 'os';
 import fs from 'fs';
 import path from 'path';
+import dl from 'ytdl-core';
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const awf = require('@craft-cloud/audiowaveform-static-aws');
+const ffmpeg = require('@ffmpeg-installer/ffmpeg').path;
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const originalStream = await fetch(`${new URL(request.url).origin}/resource/QNvsMEZqt5Y`).then((res) =>
-    res.arrayBuffer()
-  );
+  const originalStream = dl('QNvsMEZqt5Y', { filter: 'audioonly', quality: 'highestaudio' });
 
-  const inputPath = path.join(os.tmpdir(), 'input.webm');
+  const inputPath1 = path.join(os.tmpdir(), 'input.webm');
+  const inputPath2 = path.join(os.tmpdir(), 'input.mp3');
   const outputPath1 = path.join(os.tmpdir(), 'test.dat');
   const outputPath2 = path.join(os.tmpdir(), 'test.png');
-  await fs.promises.writeFile(inputPath, Buffer.from(originalStream));
+  await fs.promises.writeFile(inputPath1, originalStream);
 
   {
-    const { stdout, stderr } = await exec(`${awf()} -i ${inputPath} -o ${outputPath1} --input-format ogg`);
+    const { stdout, stderr } = await exec(`${ffmpeg} -i ${inputPath1} ${inputPath2}`);
+    console.log('stdout:', stdout);
+    console.error('stderr:', stderr);
+  }
+
+  {
+    const { stdout, stderr } = await exec(`${awf()} -i ${inputPath2} -o ${outputPath1}`);
     console.log('stdout:', stdout);
     console.error('stderr:', stderr);
   }
