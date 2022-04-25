7e612e52bea60254c18c07abc108e68829b5c9f9
Mayank <mayank99@users.noreply.github.com>
2022-04-25 17:40:39 -0400

    specify opus format

--- app/routes/resource/audiowaveform.ts
+++ app/routes/resource/audiowaveform.ts
@@ -17,15 +17,15 @@ export const loader: LoaderFunction = async () => {
   const outputPath2 = path.join(os.tmpdir(), 'test.png');
   await fs.promises.writeFile(inputPath1, originalStream);
 
-  {
-    const { stdout, stderr } = await exec(`${ffmpeg} -i ${inputPath1} -ab 128k -f mp3 ${inputPath2}`);
-    console.log('stdout:', stdout);
-    console.error('stderr:', stderr);
-  }
+  // {
+  //   const { stdout, stderr } = await exec(`${ffmpeg} -i ${inputPath1} -ab 128k -f mp3 ${inputPath2}`);
+  //   console.log('stdout:', stdout);
+  //   console.error('stderr:', stderr);
+  // }
 
   {
     const { stdout, stderr } = await exec(
-      `${awf()} -i ${inputPath2} -o ${outputPath2} -z 300 -s 60.0 -e 90.0 -w 1000 -h 200`
+      `${awf()} -i ${inputPath1} --input-format opus -o ${outputPath2} -z 300 -s 60.0 -e 90.0 -w 1000 -h 200`
     );
     console.log('stdout:', stdout);
     console.error('stderr:', stderr);
