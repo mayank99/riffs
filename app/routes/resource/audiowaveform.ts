2123a57a8a2b4ed70f8cedc9b5b06bc77499c207
Mayank <mayank99@users.noreply.github.com>
2022-05-01 11:27:36 -0400

    Remove existing files first

--- app/routes/resource/audiowaveform.ts
+++ app/routes/resource/audiowaveform.ts
@@ -7,36 +7,47 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const awf = require('@craft-cloud/audiowaveform-static-aws');
 const ffmpeg = require('@ffmpeg-installer/ffmpeg').path;
+// const spawn = require('child_process').spawn;
 
 export const loader: LoaderFunction = async () => {
   const originalStream = dl('QNvsMEZqt5Y', { filter: 'audioonly', quality: 'highestaudio' });
 
-  const inputPath1 = path.join(os.tmpdir(), 'input.opus');
+  const inputPath1 = path.join(os.tmpdir(), 'input');
   const inputPath2 = path.join(os.tmpdir(), 'input.mp3');
-  const outputPath1 = path.join(os.tmpdir(), 'test.dat');
-  const outputPath2 = path.join(os.tmpdir(), 'test.png');
+  const outputPath = path.join(os.tmpdir(), 'test.png');
+
+  [inputPath1, inputPath2, outputPath].forEach((p) => {
+    if (fs.existsSync(p)) {
+      fs.unlinkSync(p);
+    }
+  });
+
   await fs.promises.writeFile(inputPath1, originalStream);
 
-  // {
-  //   const { stdout, stderr } = await exec(`${ffmpeg} -i ${inputPath1} -ab 128k -f mp3 ${inputPath2}`);
-  //   console.log('stdout:', stdout);
-  //   console.error('stderr:', stderr);
-  // }
-
   {
-    const { stdout, stderr } = await exec(
-      `${awf()} -i ${inputPath1} --input-format opus -o ${outputPath2} -z 300 -s 60.0 -e 90.0 -w 1000 -h 200`
-    );
+    const { stdout, stderr } = await exec(`${ffmpeg} -i ${inputPath1} -f mp3 ${inputPath2}`);
     console.log('stdout:', stdout);
     console.error('stderr:', stderr);
   }
 
-  // {
-  //   const { stdout, stderr } = await exec(`${awf()} -i ${outputPath1} -o ${outputPath2}`);
-  //   console.log('stdout:', stdout);
-  //   console.error('stderr:', stderr);
-  // }
+  // await new Promise((resolve) => {
+  //   spawn(ffmpeg, ['-i', inputPath1, '-f', 'mp3', inputPath2], {
+  //     stdio: ['inherit'],
+  //   }).on('close', () => {
+  //     resolve(null);
+  //   });
+  // });
 
-  const png = await fs.promises.readFile(outputPath2);
+  console.log('ffmpeg done!');
+
+  {
+    const { stdout, stderr } = await exec(`${awf()} -i ${inputPath2} -o ${outputPath} -b 8 --pixels-per-second 4`);
+    console.log('stdout:', stdout);
+    console.error('stderr:', stderr);
+  }
+
+  console.log('audiowaveform done!');
+
+  const png = await fs.promises.readFile(outputPath);
   return new Response(png.buffer, { status: 200, headers: { 'Content-Type': 'image/png' } });
 };
