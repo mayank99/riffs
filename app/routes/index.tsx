a41aabe3c4b607c92096bdadcdc74f7e8083b644
Mayank <mayank99@users.noreply.github.com>
2022-05-08 23:21:47 -0400

    Add description paragraph

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -43,63 +43,68 @@ export default function Index() {
   };
 
   return (
-    <fetcher.Form method='post' autoComplete='off'>
-      <Combobox
-        aria-label='Search for a song or enter a url'
-        className={`search-input-wrapper ${fetcher.state !== 'idle' || transition.state !== 'idle' ? 'loading' : ''}`}
-        onSelect={(value) => {
-          const item = fetcher.data?.find((item) => item.id === value);
-          setInputValue(item?.title || '');
-          navigate(`/${value}`);
-        }}
-      >
-        <ComboboxInput
-          name='searchTerm'
-          className='search-input'
-          autocomplete={false}
-          autoComplete='off'
-          autoFocus
-          placeholder='Search for a song or enter a url'
-          value={inputValue}
-          onInput={({ currentTarget: { value } }) => {
-            doSearch(value);
-            setInputValue(value);
+    <>
+      <p>
+        Search for a song and select start/end times to create a <em>riff</em>
+      </p>
+      <fetcher.Form method='post' autoComplete='off'>
+        <Combobox
+          aria-label='Search for a song or enter a url'
+          className={`search-input-wrapper ${fetcher.state !== 'idle' || transition.state !== 'idle' ? 'loading' : ''}`}
+          onSelect={(value) => {
+            const item = fetcher.data?.find((item) => item.id === value);
+            setInputValue(item?.title || '');
+            navigate(`/${value}`);
           }}
-          onKeyDown={(e) => {
-            if (!e.isDefaultPrevented() && e.key.startsWith('Arrow')) {
-              window.requestAnimationFrame(() => {
-                listRef.current?.querySelector('[data-highlighted]')?.scrollIntoView({ block: 'nearest' });
-              });
-            }
-          }}
-        />
-        {fetcher.data && transition.state !== 'loading' && (
-          <ComboboxPopover className='search-popover'>
-            <ComboboxList className='search-list' ref={listRef}>
-              {fetcher.data.flatMap((item, index) =>
-                item.id ? (
-                  <ComboboxOption key={`${index}-${item.id}`} value={item.id!}>
-                    <Link prefetch='intent' to={`/${item.id}`} className='search-option'>
-                      <span
-                        className='search-option-thumbnail'
-                        style={{ '--thumbnail': `url(${item.thumbnail})` }}
-                        aria-hidden
-                      />
-                      <span className='search-option-title'>{item.title}</span>
-                      <span className='search-option-subtitle'>
-                        <small>{item.artists?.[0]}</small>
-                        <small>{item.duration}</small>
-                      </span>
-                    </Link>
-                  </ComboboxOption>
-                ) : (
-                  []
-                )
-              )}
-            </ComboboxList>
-          </ComboboxPopover>
-        )}
-      </Combobox>
-    </fetcher.Form>
+        >
+          <ComboboxInput
+            name='searchTerm'
+            className='search-input'
+            autocomplete={false}
+            autoComplete='off'
+            autoFocus
+            placeholder='Search…'
+            value={inputValue}
+            onInput={({ currentTarget: { value } }) => {
+              doSearch(value);
+              setInputValue(value);
+            }}
+            onKeyDown={(e) => {
+              if (!e.isDefaultPrevented() && e.key.startsWith('Arrow')) {
+                window.requestAnimationFrame(() => {
+                  listRef.current?.querySelector('[data-highlighted]')?.scrollIntoView({ block: 'nearest' });
+                });
+              }
+            }}
+          />
+          {fetcher.data && transition.state !== 'loading' && (
+            <ComboboxPopover className='search-popover'>
+              <ComboboxList className='search-list' ref={listRef}>
+                {fetcher.data.flatMap((item, index) =>
+                  item.id ? (
+                    <ComboboxOption key={`${index}-${item.id}`} value={item.id!}>
+                      <Link prefetch='intent' to={`/${item.id}`} className='search-option'>
+                        <span
+                          className='search-option-thumbnail'
+                          style={{ '--thumbnail': `url(${item.thumbnail})` }}
+                          aria-hidden
+                        />
+                        <span className='search-option-title'>{item.title}</span>
+                        <span className='search-option-subtitle'>
+                          <small>{item.artists?.[0]}</small>
+                          <small>{item.duration}</small>
+                        </span>
+                      </Link>
+                    </ComboboxOption>
+                  ) : (
+                    []
+                  )
+                )}
+              </ComboboxList>
+            </ComboboxPopover>
+          )}
+        </Combobox>
+      </fetcher.Form>
+    </>
   );
 }
