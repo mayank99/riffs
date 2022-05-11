929af06bbee70de72bfbcb7496e81736093e0056
Mayank <mayank99@users.noreply.github.com>
2022-05-10 21:40:52 -0400

    Fix anchor

--- app/helpers/Button.tsx
+++ app/helpers/Button.tsx
@@ -5,7 +5,7 @@ import styles from './Button.css';
  * A button that supports primary (cta), secondary (outline), and loading styles.
  * Can be rendered as a button or as a link if `href` is passed
  */
-export const Button: Overloaded = ({ loading, variant = 'cta', children, className, onClick, href, ...rest }) => {
+export const Button: Overloaded = ({ loading, variant = 'cta', children, className, ...rest }) => {
   const props: (ButtonProps | AnchorProps) & { [k: string]: unknown } = {
     className: `button ${variant === 'outline' ? 'outline' : ''} ${className ?? ''}`,
     'data-state': loading ? 'loading' : undefined,
